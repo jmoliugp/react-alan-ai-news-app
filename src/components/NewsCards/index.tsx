f5b0809d8b6312d0054718cf4f4f5aed4143113e
Solo <juan@xmartlabs.com>
2023-09-18 23:06:22 -0300

    news card list component

---
+++ src/components/NewsCards/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { NewsArticle } from "../../entities";
+import { NewsCard } from "../NewsCard";
+import { Grid, Grow, Typography } from "@material-ui/core";
+
+import useStyles from "./styles";
+
+interface Props {
+  articles: NewsArticle[];
+}
+
+export const NewsCards: React.FC<Props> = (props) => {
+  const classes = useStyles();
+
+  return (
+    <Grow in>
+      <Grid
+        className={classes.container}
+        container
+        alignItems="stretch"
+        spacing={3}
+      >
+        {props.articles.map((article, index) => {
+          return (
+            <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: "flex" }}>
+              <NewsCard article={article} index={index} />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Grow>
+  );
+};
---
+++ src/components/NewsCards/styles.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core";
+
+const styles = makeStyles({
+  container: {
+    margin: 0,
+    padding: "0 5%",
+    width: "100%",
+  },
+});
+
+export default styles;
