68c81f8caf186d264261d9b346d4691d3ae66af3
Solo <juan@xmartlabs.com>
2023-09-19 10:43:48 -0300

    info cards within news cards board

--- src/components/NewsCards/styles.ts
+++ src/components/NewsCards/styles.ts
@@ -1,11 +1,25 @@
 import { makeStyles } from "@material-ui/core";
 
-const styles = makeStyles({
+export const useStyles = makeStyles({
+  card: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    height: "45vh",
+    padding: "10%",
+    borderRadius: 10,
+    color: "white",
+  },
+  infoCard: {
+    display: "flex",
+    flexDirection: "column",
+    textAlign: "center",
+  },
   container: {
-    margin: 0,
     padding: "0 5%",
     width: "100%",
+    margin: 0,
   },
 });
-
-export default styles;
