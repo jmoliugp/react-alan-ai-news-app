c2c5165867842fd44a66e77e8f7fdd028d3a5962
Solo <juan@xmartlabs.com>
2023-09-23 15:02:55 -0300

    removes unused styles

--- src/components/NewsCards/styles.ts
+++ src/components/NewsCards/styles.ts
@@ -1,22 +1,6 @@
 import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles({
-  card: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "100%",
-    height: "45vh",
-    padding: "10%",
-    borderRadius: 10,
-    color: "white",
-  },
-  infoCard: {
-    display: "flex",
-    flexDirection: "column",
-    textAlign: "center",
-  },
   container: {
     padding: "0 5%",
     width: "100%",
